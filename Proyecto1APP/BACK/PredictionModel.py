44aa80ff5c7af62a7799c8bc8c009cd6ead0d4cf
itsmemanuu <m.pachecom2@uniandes.edu.co>
2024-04-18 15:54:39 -0500

    AAAAAAAAAAAAH

--- Proyecto1APP/BACK/PredictionModel.py
+++ Proyecto1APP/BACK/PredictionModel.py
@@ -4,6 +4,7 @@ class Model:
 
     def __init__(self):
         self.model = load('assets/modelo.joblib')
+        
     
     def makePredictions(self, data):
         return self.model.predict(data)
